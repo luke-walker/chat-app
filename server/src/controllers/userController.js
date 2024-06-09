bce1393d31ddd0d417cd4ed454a8a9b4c043b138
Luke Walker <lukeryanwalker2000@gmail.com>
2024-06-08 18:47:24 -0700

    Fixed getUserData response

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -4,7 +4,7 @@ export async function getUserData(req, res) {
     try {
         const email = req.params.email;
         const userData = await userModel.find({email});
-        return res.json(userData);
+        return res.status(200).json(userData.length > 0 ? userData[0] : []);
     } catch (err) {
         return res.sendStatus(500);
     }
